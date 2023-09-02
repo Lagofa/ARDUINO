30d52aeb318093c4ed8ebf11b74bcd3e7caa1f46
Fabian <falago@gmail.com>
2023-09-02 10:49:12 -0300

    Update config.h

--- config/config.h
+++ config/config.h
@@ -1,7 +1,7 @@
 #include "../sensors/sensorDS18B20.hpp"
 
-const char* SSID = "3d4ab2";
-const char* PASSWORD =  "perestroica";
+const char* SSID = "";
+const char* PASSWORD =  "";
 const char* URL = "https://hidden-springs-61949.herokuapp.com/api/measurements";
 const char* SSL_FINGERPRINT="4C 55 4D FA EF B6 8F 4B A4 20 50 54 EA 57 18 36 89 66 F0 08";
 const char* uuid = "sensor1";
